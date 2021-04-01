fd5c3dc399b4ba64b7e1e3530da807c658153cdb
YodJI <yodji62@yandex.ru>
2021-04-01 17:34:52 +0300

    На проверку 3/4

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const { errors } = require("celebrate");
-const { celebrate, Joi } = require("celebrate");
-const cors = require("cors");
-const userRouter = require("./routes/users");
-const cardsRouter = require("./routes/cards");
-const { login, createProfile } = require("./controllers/users");
-const auth = require("./middlewares/auth");
-const NotFoundError = require("./errors/NotFoundError");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const cors = require('cors');
+const userRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { login, createProfile } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 const PORT = 3000;
 
 const corsOptions = {
-  origin: ["*"],
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  origin: ['*'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ["Content-Type", "origin", "Authorization"],
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
 };
-app.use("*", cors(corsOptions));
+app.use('*', cors(corsOptions));
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -33,38 +33,38 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 
 app.use(bodyParser.json());
 app.use(requestLogger);
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Сервер сейчас упадёт");
+    throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
 // авторизация
 app.post(
-  "/signin",
+  '/signin',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().alphanum().required().min(8),
     }),
   }),
-  login
+  login,
 );
 // регистрация
 app.post(
-  "/signup",
+  '/signup',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().alphanum().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(/^https?:\/\/[a-z0-9\W]+#?$/i, "url"),
+      avatar: Joi.string().pattern(/^https?:\/\/[a-z0-9\W]+#?$/i, 'url'),
     }),
   }),
-  createProfile
+  createProfile,
 );
-app.use("/", auth, userRouter);
-app.use("/", auth, cardsRouter);
+app.use('/', auth, userRouter);
+app.use('/', auth, cardsRouter);
 app.use(() => {
   throw new NotFoundError('"Запрашиваемый ресурс не найден"');
 });
@@ -74,7 +74,7 @@ app.use((err, req, res) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
-    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
 });
 
