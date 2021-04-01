c58d5eb699ddf2a0ab7b526b5b189c1ef20cf76e
YodJI <yodji62@yandex.ru>
2021-04-02 01:36:50 +0300

    Test

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-const PORT = 3001;
+const { PORT = 3000 } = process.env;
 
 const corsOptions = {
   origin: ['*'],
